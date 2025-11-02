124e1fb6f1e04b5c35f6ddc37352fb89e45a516e
Сальников Евгений Владимирович <salnikov_e_v@m00office0207.local>
2025-11-02 12:58:21 +0300

    contacts layout wip

--- app/src/main/java/otus/gpb/homework/viewandresources/ContactsActivity.kt
+++ app/src/main/java/otus/gpb/homework/viewandresources/ContactsActivity.kt
@@ -1,11 +1,64 @@
 package otus.gpb.homework.viewandresources
 
+import android.icu.text.SimpleDateFormat
+import android.icu.util.TimeZone
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.datepicker.MaterialStyledDatePickerDialog
+import com.google.android.material.textfield.TextInputEditText
+import java.util.Date
+import java.util.Locale
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
 class ContactsActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacts)
+
+        findViewById<MaterialToolbar>(R.id.contacts_toolbar).setNavigationOnClickListener { finish() }
+
+        val bookingDateEditText = findViewById<TextInputEditText>(R.id.booking_date_edit_text)
+
+
+
+        val datePickerBuilder = MaterialDatePicker.Builder.datePicker()
+        datePickerBuilder.setTitleText("Select Date")
+        if (bookingDateEditText.text.toString().isNotEmpty()) {
+            try {
+                val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+                dateFormat.timeZone = TimeZone.getTimeZone("UTC")
+                val parsedDate = dateFormat.parse(bookingDateEditText.text.toString())
+                parsedDate?.let {
+                    datePickerBuilder.setSelection(it.time)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+        val datePicker = datePickerBuilder.build()
+        datePicker.addOnPositiveButtonClickListener { selection ->
+            val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+            dateFormat.timeZone = TimeZone.getTimeZone("UTC")
+            val formattedDate = dateFormat.format(Date(selection))
+            bookingDateEditText.setText(formattedDate)
+        }
+        datePicker.addOnNegativeButtonClickListener {
+            bookingDateEditText.clearFocus()
+        }
+        datePicker.addOnDismissListener {
+            bookingDateEditText.clearFocus()
+        }
+
+        bookingDateEditText.setOnClickListener {
+            datePicker.show(supportFragmentManager, "date_picker_tag")
+        }
+
     }
 }
