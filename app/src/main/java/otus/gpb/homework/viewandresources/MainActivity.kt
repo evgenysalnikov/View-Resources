e0716b3a2129b93e2c69a9e8d1b967ada2f97de0
Сальников Евгений Владимирович <salnikov_e_v@m00office0207.local>
2025-11-12 21:06:52 +0300

    dialog

--- app/src/main/java/otus/gpb/homework/viewandresources/MainActivity.kt
+++ app/src/main/java/otus/gpb/homework/viewandresources/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,10 +19,23 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, CartActivity::class.java))
         }
 
-        findViewById<Button>(R.id.signin_button).setOnClickListener {
-            MaterialAlertDialogBuilder(this)
-                .setView(R.layout.dialog_signin)
-                .show()
+
+        val builder = MaterialAlertDialogBuilder(this)
+        val view = layoutInflater.inflate(R.layout.dialog_signin, null)
+        builder.setView(view)
+        val dialog = builder.create()
+
+        view.findViewById<Button>(R.id.password_reset).setOnClickListener {
+            view.findViewById<TextInputEditText>(R.id.password_field).setText("")
         }
+
+        view.findViewById<Button>(R.id.cancel_btn).setOnClickListener { view ->
+            dialog.dismiss()
+        }
+
+        findViewById<Button>(R.id.signin_button).setOnClickListener {
+            dialog.show()
+        }
+
     }
 }
